bba3e796a1e3bfe1516ee7e41b8a1356dc1bf9c0
Asrat <aabebe.navy@gmail.com>
2023-12-01 16:23:10 -0800

    Seperate test for length of enjoys added

--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -11,8 +11,13 @@ RSpec.describe Cat, type: :model do
     expect(cat.errors[:age]).to_not be_empty
   end
 
-  it "should validate the cat enjoys a hobby and the minimum length is 10" do
+  it "should validate the cat enjoys a hobby " do
     cat = Cat.create(name: 'Bob', age: 1, image: 'https://hips.hearstapps.com/hmg-prod/images/cute-cat-photos-1593441022.jpg?crop=0.670xw:1.00xh;0.167xw,0&resize=640:*')
+    expect(cat.errors[:enjoys]).to_not be_empty
+  end
+
+  it "should validate the cat enjoys a hobby and the minimum length is 10" do
+    cat = Cat.create(name: 'Bob', enjoys: 'Climbing', age: 1, image: 'https://hips.hearstapps.com/hmg-prod/images/cute-cat-photos-1593441022.jpg?crop=0.670xw:1.00xh;0.167xw,0&resize=640:*')
     expect(cat.errors[:enjoys]).to_not be_empty
   end
 
